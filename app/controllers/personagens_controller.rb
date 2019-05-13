e97d3d5e01eed2369329b3235d4abe28b9f5fdff
Matheus <matheusldasilva20088@gmail.com>
2019-05-13 01:24:53 -0300

    funcao new e html

---
+++ app/controllers/personagens_controller.rb
@@ -0,0 +1,10 @@
+class PersonagensController < ApplicationController
+
+  def new
+    @personagem = Personagem.new
+  end
+
+  
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  #root 'home#index'
+
+  resources :poderes
+  resources :personagens
+end
