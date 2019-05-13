e94103de8c3de8ffca1985269782ead43a2f0af9
Matheus <matheusldasilva20088@gmail.com>
2019-05-13 01:53:45 -0300

    html show e funcao

--- app/controllers/personagens_controller.rb
+++ app/controllers/personagens_controller.rb
@@ -4,7 +4,29 @@ class PersonagensController < ApplicationController
     @personagem = Personagem.new
   end
 
-  
+  def create
+    @personagem = Personagem.new(personagem_params)
+
+    if @personagem.save
+      redirect_to @personagem,
+                  notice: "Personagem criado com sucesso"
+        else
+          render action: :new
+        end
+
+
+  end
+
+  def personagem_params
+
+    params.require(:personagem).permit(:nome, :forca, :resistencia, :agilidade, :destreza)
+  end
+
+
+  def show
+    @personagem = Personagem.find(params[:id])
+
+  end
 
 
 end
